76f33445697ec97ca2c2872e6efaafbe8a4afcb2
@Klisman_12 <138720438+EmanuelKlisman@users.noreply.github.com>
2025-11-20 12:31:53 +0000

    trabalho faculdade

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int i = 10;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // For de 5 a 9
+    for (i = 5; i < 10; i++) {
+        printf("A rainha foi para a direita\n");
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // While até i ser menor ou igual a 10
+    while (i <= 10) {
+        printf("A rainha foi para a Esquerda\n");
+        i++; // incrementa 
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    // Do While executa ao menos 1 vez
+    do {
+        printf("Cima\n");
+        i++;
+    } while (i == 10); // não entrará novamente pois i foi incrementado
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    printf("Desafio Xadrez\n");
 
     return 0;
 }
