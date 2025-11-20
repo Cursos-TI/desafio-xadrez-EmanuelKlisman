cabb9317745c95207a5a27e170e697499e4454da
@Klisman_12 <138720438+EmanuelKlisman@users.noreply.github.com>
2025-11-20 12:35:38 +0000

    trabalho faculdade alterado

--- xadrez.c
+++ xadrez.c
@@ -4,22 +4,22 @@ int main() {
 
     int i = 10;
 
-    // For de 5 a 9
+    // For para 5 a 9
     for (i = 5; i < 10; i++) {
         printf("A rainha foi para a direita\n");
     }
 
-    // While até i ser menor ou igual a 10
+    //  i ser menor ou igual a 10
     while (i <= 10) {
         printf("A rainha foi para a Esquerda\n");
         i++; // incrementa 
     }
 
-    // Do While executa ao menos 1 vez
+    // 
     do {
         printf("Cima\n");
         i++;
-    } while (i == 10); // não entrará novamente pois i foi incrementado
+    } while (i == 10); // 
 
     printf("Desafio Xadrez\n");
 
